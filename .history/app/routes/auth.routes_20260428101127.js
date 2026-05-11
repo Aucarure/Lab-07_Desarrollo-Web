ff7eb338c584e359b114a669f980e5ce341d8c87
Victor Manuel Aucarure Oliva <olivavictor936@gmail.com>
2026-05-10 20:14:21 -0500

    Removemos node_modules y .env

--- .history/app/routes/auth.routes_20260428101127.js
+++
@@ -1,9 +0,0 @@
-import { signup, signin } from "../controllers/auth.controller.js";
-import { checkDuplicateUsernameOrEmail } from "../middleware/verifySignUp.js";
-
-
-export default function(app) {
-  app.post("/api/auth/signup", checkDuplicateUsernameOrEmail, signup);
-  app.post("/api/auth/signin", signin);
-}
-
